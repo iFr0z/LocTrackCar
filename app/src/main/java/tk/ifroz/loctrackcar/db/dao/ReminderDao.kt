96206ad279a4c2e68e7266f04ede4be9d127af2b
iFr0z <ifr0z.dev@outlook.com>
2019-06-03 17:06:17 +0700

    refactor(ReminderDao): optimization imports

--- app/src/main/java/tk/ifroz/loctrackcar/db/dao/ReminderDao.kt
+++ app/src/main/java/tk/ifroz/loctrackcar/db/dao/ReminderDao.kt
@@ -1,7 +1,10 @@
 package tk.ifroz.loctrackcar.db.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
 import tk.ifroz.loctrackcar.db.entity.Reminder
 
 @Dao
