a4c1f7d4c2d912b6d50557514ccf8bf8cdd9cc6e
iFr0z <ifr0z.dev@outlook.com>
2019-06-17 16:04:56 +0700

    feature: init unique work with policy for geocoder

--- app/src/main/java/tk/ifroz/loctrackcar/viewmodel/GeocoderViewModel.kt
+++ app/src/main/java/tk/ifroz/loctrackcar/viewmodel/GeocoderViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.work.*
+import androidx.work.ExistingWorkPolicy.REPLACE
 import androidx.work.NetworkType.CONNECTED
 import tk.ifroz.loctrackcar.ui.work.GeocoderWork
 
@@ -11,17 +12,17 @@ class GeocoderViewModel(application: Application) : AndroidViewModel(application
 
     private val workManager: WorkManager = WorkManager.getInstance(application)
     internal val outputStatus: LiveData<List<WorkInfo>>
-        get() = workManager.getWorkInfosByTagLiveData(DATA_ID)
+        get() = workManager.getWorkInfosForUniqueWorkLiveData(GEOCODER_WORK)
 
     internal fun getStreetName(data: Data) {
         val constraints = Constraints.Builder().setRequiredNetworkType(CONNECTED).build()
-        val streetNameWork = OneTimeWorkRequest.Builder(GeocoderWork::class.java).addTag(DATA_ID)
+        val streetNameWork = OneTimeWorkRequest.Builder(GeocoderWork::class.java)
             .setInputData(data).setConstraints(constraints).build()
 
-        workManager.enqueue(streetNameWork)
+        workManager.beginUniqueWork(GEOCODER_WORK, REPLACE, streetNameWork).enqueue()
     }
 
     companion object {
-        const val DATA_ID = "LocTrackCar_street_data_id"
+        const val GEOCODER_WORK = "LocTrackCar_geocoder_work"
     }
 }
