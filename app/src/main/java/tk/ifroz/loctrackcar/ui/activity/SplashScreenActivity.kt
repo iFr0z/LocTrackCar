09bd362c084baddff1e8d8bfa330a8a84cdc1b6f
iFr0z <ifr0z.dev@outlook.com>
2020-06-23 11:53:35 +0700

    feature: add transition listener extension

--- app/src/main/java/tk/ifroz/loctrackcar/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/tk/ifroz/loctrackcar/ui/activity/SplashScreenActivity.kt
@@ -3,8 +3,8 @@ package tk.ifroz.loctrackcar.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.splash_screen_activity.*
+import ru.ifr0z.core.extension.transitionListener
 import tk.ifroz.loctrackcar.R
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -17,17 +17,9 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun userInterface() {
-        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
-            }
-
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
-
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
-
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
-        })
+        motionLayout.transitionListener(
+            this@SplashScreenActivity, Intent(this@SplashScreenActivity, MainActivity::class.java)
+        )
     }
 
     override fun onResume() {
---
+++ core/src/main/java/ru/ifr0z/core/extension/TransitionListenerExt.kt
@@ -0,0 +1,21 @@
+package ru.ifr0z.core.extension
+
+import android.content.Context
+import android.content.Intent
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+fun MotionLayout.transitionListener(context: Context, intent: Intent) {
+    setTransitionListener(
+        object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                context.startActivity(intent)
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+        }
+    )
+}
