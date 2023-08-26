bf77a66f7f9a9ac531a5812ae1aff6e412786408
iFr0z <ifr0z.dev@outlook.com>
2023-08-27 06:35:59 +0700

    feature: init data object

--- app/src/main/java/tk/ifroz/loctrackcar/ui/intent/MainIntent.kt
+++ app/src/main/java/tk/ifroz/loctrackcar/ui/intent/MainIntent.kt
@@ -2,5 +2,5 @@ package tk.ifroz.loctrackcar.ui.intent
 
 sealed class MainIntent {
 
-    object FetchAddress : MainIntent()
+    data object FetchAddress : MainIntent()
 }
--- app/src/main/java/tk/ifroz/loctrackcar/ui/viewstate/MainState.kt
+++ app/src/main/java/tk/ifroz/loctrackcar/ui/viewstate/MainState.kt
@@ -2,8 +2,8 @@ package tk.ifroz.loctrackcar.ui.viewstate
 
 sealed class MainState<out T : Any> {
 
-    object Idle : MainState<Nothing>()
-    object Loading : MainState<Nothing>()
+    data object Idle : MainState<Nothing>()
+    data object Loading : MainState<Nothing>()
     data class Success<out T : Any>(val data: T) : MainState<T>()
     data class Error(val error: String?) : MainState<Nothing>()
 }
