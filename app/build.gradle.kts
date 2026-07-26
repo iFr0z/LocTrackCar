c4f32d7a09ef2d77e81a52be3ae9542479772c5e
iFr0z <i@fr0z.ru>
2026-07-26 16:46:44 +0700

    New version 10.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "tk.ifroz.LocTrackCar"
         minSdk = 26
         targetSdk = 37
-        versionCode = 87
-        versionName = "10.2.0"
+        versionCode = 88
+        versionName = "10.3.0"
     }
 
     buildFeatures {
